25e7725c261e1759a7cff953446e962a5d247393
Mirhanda <mhopealewine@gmail.com>
2022-03-23 10:06:15 -0400

    Initial commit for MovingEyes

---
+++ eyes.js
@@ -0,0 +1,13 @@
+//recall the div element from html file
+var balls = document.getElementsByClassName("ball");
+
+document.onmousemove = () => {
+  var x = (event.clientX * 100) / window.innerWidth + "%";
+  var y = (event.clientY * 100) / window.innerHeight + "%";
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transfoorm = "translate(-" + x + ",-" + y + ")";
+  }
+};
